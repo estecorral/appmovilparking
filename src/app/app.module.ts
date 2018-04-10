695f21f7d5a507d3a2018ba484a46d1eb119615e
Esteban Corral <estecorral@gmail.com>
2018-04-10 12:57:45 +0200

    Cambia estilos página de login , configuraciones firebase y añade servicio usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { firebaseConfig} from "../config/firebase.config";
+
+// servicios
+import {UsuarioService} from "../providers/usuario/usuario";
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +23,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +36,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UsuarioService
   ]
 })
 export class AppModule {}
---
+++ src/config/firebase.config.ts
@@ -0,0 +1,9 @@
+// Archivo de configuración para Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyCNfSpb7qP_CsUwFB6qNjQ5naa-QJghPms",
+  authDomain: "tfgparking.firebaseapp.com",
+  databaseURL: "https://tfgparking.firebaseio.com",
+  projectId: "tfgparking",
+  storageBucket: "tfgparking.appspot.com",
+  messagingSenderId: "969295892053"
+};
